8d040505e0a9f5b6506824e13d5e18bd5bdda328
jeffngugi <ngugimwathi254@gmail.com>
2023-02-27 22:14:00 +0300

    jeff ngugi

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 import {AppRegistry} from 'react-native';
 // import App from './App';
 // import App from './Playground';
-import App from './App2'
+import App from './App2';
 import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
 
